d365bdf16d88c3a583be7d5d61a1e6220705cfa3
Eloise Barrow <eloisebarrow@Eloises-Air.home>
2019-06-13 18:22:16 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,20 +1,44 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "king-of-hearts.png"
+}
+];
 var cardsInPlay = [];
 
 function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) { // if cards 1 and 2 are equal
-		console.log('You found a match!'); // tell user they found a match
+		alert('You found a match!'); // tell user they found a match
 	} else {
-		console.log('Sorry, try again.'); // ELSE tell user to try again
+		alert('Sorry, try again.'); // ELSE tell user to try again
 	}
 };
 
 function flipCard(cardId) {
-	cardsInPlay.push(cards[cardId]);
-	console.log("User flipped " + cards[cardId]); // Log which card the user flipped to the console
+	cardsInPlay.push(cards[cardId].rank);
+	console.log("User flipped " + cards[cardId].rank); // Log which card the user flipped to the console
 	if (cardsInPlay.length === 2){ // if the user has flipped 2 cards
 		checkForMatch();
 	}
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 };
 // Note checkForMatch() must come before flipCard() so we can use the former in the latter
 flipCard(0);
